b600f10db1b007ce96ce6ad772cbbba54f9bca2b
Vitaliy Borodin <borodinvitaliy@gmail.com>
2015-03-31 23:31:40 +0300

    Set port for Heroku

--- server.js
+++ server.js
@@ -177,5 +177,6 @@
     });
 
     // listen
-    app.listen(8080);
-    console.log("App listening on port 8080");
+    var port = process.env.PORT || 8080;
+    app.listen(port)
+    console.log("App listening on port " + port);
